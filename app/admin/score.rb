19c0f9f352265d79f3a1e245eeac235d2987d247
Chris Jewell <chrisjohnjewell@gmail.com>
2015-05-24 15:48:17 +1000

    Basic AA config and setup for Exercise and Score

--- app/admin/score.rb
+++ app/admin/score.rb
@@ -1,17 +1,9 @@
 ActiveAdmin.register Score do
+  permit_params \
+    :user_id,
+    :exercise_id,
+    :score,
+    :discipline
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+  # TODO discipline as select, user email in user select
 end
