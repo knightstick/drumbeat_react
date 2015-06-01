d9e649d7dc051a9aba4b9517118cdf65a36cc697
Chris Jewell <chrisjohnjewell@gmail.com>
2015-06-01 20:54:04 +1000

    Add first attempt unlock UI - just for demo purposes

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,4 +6,17 @@ class ExercisesController < ApplicationController
   def show
     @exercise = Exercise.find(params[:id])
   end
+
+  def unlocks
+    # TODO: refactor into class method?
+    # Maybe a Facade with tiers etc?
+    all_exercises = Exercise.all
+    @locked_exercises = all_exercises.select {|exercise| !current_user.exercises.include? exercise }
+  end
+
+  def unlock
+    exercise = Exercise.find(params[:id])
+    Unlock.unlock(current_user, exercise)
+    redirect_to exercises_path
+  end
 end
