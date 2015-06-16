de112136c82c58d13c60b2898f88c116afe25a66
Chris Jewell <chrisjohnjewell@gmail.com>
2015-06-16 20:24:08 +1000

    Add basic POST score functionality

--- app/assets/javascripts/components/exercise.js.jsx
+++ app/assets/javascripts/components/exercise.js.jsx
@@ -4,7 +4,7 @@ var Exercise = React.createClass({
     return(
       <div className='exercise'>
         <ExerciseBody exercise={this.props.exercise} />
-        <Scorecard exercise={this.props.exercise} />
+        <Scorecard exercise={this.props.exercise} url={this.props.url} />
       </div>
     );
   }
@@ -26,26 +26,63 @@ var ExerciseBody = React.createClass({
   }
 });
 
+
+// TODO: try and use data more...
 var Scorecard = React.createClass({
+  handleScoreSubmit: function(score) {
+    this.setState({data: score}, function() {
+      // `setState` accepts a callback. To avoid (improbable) race condition,
+      // `we'll send the ajax request right after we optimistically set the new
+      // `state.
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        type: 'POST',
+        data:
+          {
+            score: {
+              discipline: '4_bars',
+              score: this.state.data.score
+            }
+          },
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          debugger
+        }.bind(this)
+      });
+    });
+  },
 
   render: function() {
     return(
-      <ScoreWidget exercise={this.props.exercise} />
+      <ScoreWidget onScoreSubmit={this.handleScoreSubmit}/>
     )
   }
 });
 
 var ScoreWidget = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var score = React.findDOMNode(this.refs.score).value.trim();
+    if (!score) {
+      return
+    }
+    // Should discipline be state?
+    this.props.onScoreSubmit({score: score, discipline: '4_bars'})
+    React.findDOMNode(this.refs.score).value = '';
+    React.findDOMNode(this.refs.score).placeholder = score;
+  },
 
+  // TODO: Set placholder equal to score, Make input look nice
   render: function() {
     return(
       <div className='panel'>
-        <form>
+        <form className="scoreForm" onSubmit={this.handleSubmit}>
           <div className="input-group">
-            <input type="text" className="form-control" placeholder="120" />
-            <span className="input-group-btn">
-              <button className="btn btn-default btn-danger" type="button">Score!</button>
-            </span>
+            <input type="text" className="form-control" placeholder="120" ref="score" />
+            <input type="submit" value="Score" name="Score" />
           </div>
         </form>
       </div>
